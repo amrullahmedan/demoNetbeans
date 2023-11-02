996eb2fe0159b7d6f5d228297fc27d557363e15f
Amrullah <78847965+amrullahmedan@users.noreply.github.com>
2023-11-02 14:55:09 +0700

    Add files via upload

---
+++ PUTRI/src/main/java/com/CHAIRUNISAH/PUTRI.java
@@ -0,0 +1,160 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.CHAIRUNISAH;
+
+
+import java.util.Scanner;
+public class PUTRI {
+
+    public static void main(String[] args) {
+        double Nilai1, Nilai2, hasil;
+        boolean hasil1;
+        
+        Scanner keyboard = new Scanner(System.in);
+        //
+//        ARITMATIKA
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 =keyboard.nextInt();
+//////
+////        // penjumlahan
+//        hasil = Nilai1 + Nilai2;
+//        System.out.println("Hasil = " + hasil);
+////
+//        System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 = keyboard.nextInt();
+//////
+//        // pengurangan
+//        hasil = Nilai1 - Nilai2;
+//        System.out.println("Hasil = " + hasil);
+////
+//         System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 = keyboard.nextInt();
+//////
+////        // perkalian
+//        hasil = Nilai1 * Nilai2;
+//        System.out.println("Hasil = " + hasil);
+////
+////
+//        System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 = keyboard.nextInt();
+////
+////        // Pembagian
+//        hasil = Nilai1 / Nilai2;
+//        System.out.println("Hasil = " + hasil);
+////
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 = keyboard.nextInt();
+//
+//        // Sisa Bagi
+//        hasil = Nilai1 % Nilai2;
+//        System.out.println("Hasil = " + hasil);
+//        
+////        OPERATOR PENUGASAN
+//           
+//        System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 = keyboard.nextInt();
+//        // Pengisian dan Penambahan
+//        hasil = Nilai1 += Nilai2;
+//        System.out.println("OPERATO PENUGASAN");
+//        System.out.println("Hasil += " + hasil);
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 = keyboard.nextInt();
+//        // Pengisian dan Pengurangan
+//        hasil = Nilai1 -= Nilai2;
+//        System.out.println("OPERATO PENUGASAN");
+//        System.out.println("Hasil -= " + hasil);
+//
+//
+////          OPERATOR PEMBANDING
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai1 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai2 = keyboard.nextInt();
+//
+//        // Lebih besar
+//        hasil1 = Nilai1 > Nilai2;
+//        System.out.println("OPERATOR BOOLEAN");
+//        System.out.println("Nilai1 > Nilai2 = " + hasil1);
+//        
+
+//          OPERATOR BITWISE
+        
+        
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai4 = keyboard.nextInt();
+//        // Bitwise and (&)
+//        hasil3 = Nilai3 & Nilai4;
+//        System.out.println("Bitwise and (&)");
+//        System.out.println("Nilai3 & Nilai4 = " + hasil3);
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai4 = keyboard.nextInt();
+//        // Bitwise OR (|)
+//        hasil3 = Nilai3 | Nilai4;
+//        System.out.println("Bitwise OR (|)");
+//        System.out.println("Nilai3 | Nilai4 = " + hasil3);
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai4 = keyboard.nextInt();
+//        // Bitwise XOR (|)
+//        hasil3 = Nilai3 ^ Nilai4;
+//        System.out.println("Bitwise OR (|)");
+//        System.out.println("Nilai3 ^ Nilai4 = " + hasil3);
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai4 = keyboard.nextInt();
+//        // Bitwise Negasi (~)
+//        hasil3 = ~ Nilai4;
+//        System.out.println("Bitwise Negasi (~)");
+//        System.out.println("~Nilai4 = " + hasil3);
+//        
+        int Nilai3, Nilai4, hasil3;
+        System.out.print("Input angka-1: ");
+        Nilai3 = keyboard.nextInt();
+        System.out.print("Input angka-2: ");
+        Nilai4 = keyboard.nextInt();
+        // Bitwise Negasi (~)
+        hasil3 = Nilai4 << Nilai3; 
+        System.out.println("Bitwise Left Shift");
+        System.out.println("Hasil Left Shift " + hasil3);
+        
+       
+        System.out.print("Input angka-1: ");
+        Nilai3 = keyboard.nextInt();
+        System.out.print("Input angka-2: ");
+        Nilai4 = keyboard.nextInt();
+        // Bitwise Negasi (~)
+        hasil3 = Nilai4 >> Nilai3; 
+        System.out.println("Bitwise Left Shift");
+        System.out.println("Hasil Left Shift " + hasil3);
+
+        
+    }
+}
